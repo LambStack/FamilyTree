b34e4c012f012a28883a3fa0af5b6ff6dac717b9
Joseph Agnelli <jxa4717@rit.edu>
2022-09-02 15:23:31 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import PersonNode from './data/PersonNode';
+import PersonCard from './components/PersonCard.react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let rootPersonNode = PersonNode.generateData();
+	// PersonNode.logData(rootPersonNode);
+
+	const cardList = [];
+
+	function genComponentList(node) {
+		cardList.push(<PersonCard personNode={node} key={node.id} />);
+		for (let child of node.children) {
+			genComponentList(child);
+		}
+	}
+	genComponentList(rootPersonNode);
+	return <>{cardList}</>;
 }
 
 export default App;
